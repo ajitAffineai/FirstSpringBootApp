5e2d917ff1ded63de91640c5dfc404458791fe36
ajitAffineai <127475369+ajitAffineai@users.noreply.github.com>
2023-07-12 15:31:06 +0530

    path Variable added

--- src/main/java/com/example/demo/HelloWorldController.java
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -34,6 +36,26 @@ public class HelloWorldController {
 		Students.add(new StudentsInfo("Ram","chalec",19));
 		return Students;
 	}
-	
-}
 
+	// PathVariable annotation
+	// http://localhost:8080/Ramesh/c/21
+	@GetMapping("/student/{firstName}/{lastName}/{age}")
+	public StudentsInfo StudentPathVariable(
+			@PathVariable("firstName") String firstName1,
+			@PathVariable("lastName") String lastName1,
+			@PathVariable("age") int age1
+			) {
+		return new StudentsInfo(firstName1,lastName1,age1);
+	}
+
+
+	// Rest API to handle Query Parameter
+	@GetMapping("/students/query")
+	public StudentsInfo StudentQueryParam(
+			@RequestParam(name = "firstName") String firstName1,
+			@RequestParam(name = "lastName") String lastName1,
+			@RequestParam(name = "age") int age1
+			) {
+	return new StudentsInfo(firstName1,lastName1,age1);
+	}
+}
